d29c3e105ecfac55268973f4ea6d0db3c06412b9
Andrew Dennis <adennis4@gmail.com>
2012-03-12 11:57:36 -0700

    Valid URLs and title

---
+++ app/models/link.rb
@@ -0,0 +1,6 @@
+class Link < ActiveRecord::Base
+  validates :url,   :presence => true, 
+                    :uniqueness => true,
+                    :format => {:with => URI.regexp}
+  validates :title, :presence => true
+end
---
+++ spec/models/link_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Link do
+  it 'requires a url' do
+    should_not be_valid
+    subject.errors[:url].should_not be_empty
+  end
+  
+  it 'requires a title' do
+    should_not be_valid
+    subject.errors[:title].should_not be_empty
+  end
+  
+  it 'does not allow duplicate urls' do
+    Link.create! :title => "dont care", :url => "http://devbootcamp.com"
+    l = Link.new :title => "dont care", :url => "http://devbootcamp.com"
+    l.should_not be_valid
+    l.errors[:url].should_not be_empty
+  end
+  
+  it 'has a valid url' do
+    l = Link.new :title => "dont care", :url => "12343254"
+    l.should_not be_valid
+  end
+end
